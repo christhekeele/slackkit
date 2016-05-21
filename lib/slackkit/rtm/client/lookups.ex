994b6d8d9dbbd959892105c7867b7db8e15681fc
Chris Keele <dev@chriskeele.com>
2016-05-20 19:31:43 -0500

    Correct lookup params ordering.

--- lib/slackkit/rtm/client/lookups.ex
+++ lib/slackkit/rtm/client/lookups.ex
@@ -64,8 +64,8 @@ defmodule Slackkit.RTM.Client.Lookups do
     @doc ~S"""
     Turns a string like `"@USER_NAME"` into the ID that Slack understands (`"U…"`).
     """
-    @spec lookup_user_id(String.t, Client.t) :: String.t
-    def lookup_user_id(name, client) do
+    @spec lookup_user_id(Client.t, String.t) :: String.t
+    def lookup_user_id(client, name) do
       lookup(client, :user, :id, name)
     end
 
@@ -74,8 +74,8 @@ defmodule Slackkit.RTM.Client.Lookups do
     direct message channel of that user (`"D…"`).  `nil` is returned if a direct
     message channel has not yet been opened.
     """
-    @spec lookup_direct_message_id(String.t, Client.t) :: String.t
-    def lookup_direct_message_id(name, client) do
+    @spec lookup_direct_message_id(Client.t, String.t) :: String.t
+    def lookup_direct_message_id(client, name) do
       lookup(client, :user, :im, name)
     end
 
@@ -83,8 +83,8 @@ defmodule Slackkit.RTM.Client.Lookups do
     Turns a string like `"@CHANNEL_NAME"` into the ID that Slack understands
     (`"C…"`).
     """
-    @spec lookup_channel_id(String.t, Client.t) :: String.t
-    def lookup_channel_id(name, client) do
+    @spec lookup_channel_id(Client.t, String.t) :: String.t
+    def lookup_channel_id(client, name) do
       lookup(client, :channel, :id, name)
     end
 
@@ -92,16 +92,16 @@ defmodule Slackkit.RTM.Client.Lookups do
     Turns a Slack user ID (`"U…"`) or direct message ID (`"D…"`) into a string in
     the format "@USER_NAME".
     """
-    @spec lookup_user_name(String.t, Client.t) :: String.t
-    def lookup_user_name(user_id, client) do
+    @spec lookup_user_name(Client.t, String.t) :: String.t
+    def lookup_user_name(client, user_id) do
       lookup(client, :user, :name, user_id)
     end
 
     @doc ~S"""
     Turns a Slack channel ID (`"C…"`) into a string in the format "#CHANNEL_NAME".
     """
-    @spec lookup_channel_name(String.t, Client.t) :: String.t
-    def lookup_channel_name(channel_id, client) do
+    @spec lookup_channel_name(Client.t, String.t) :: String.t
+    def lookup_channel_name(client, channel_id) do
       lookup(client, :channel, :name, channel_id)
     end
 
